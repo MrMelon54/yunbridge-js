08f68f06830e4056a45544fdfd721fcce12d8b65
MrMelon <github@onpointcoding.net>
2021-03-25 21:58:21 +0000

    Add the word hello for test.js

--- test.js
+++ test.js
@@ -7,5 +7,5 @@ const YunBridge = requre('./index');
   let a1 = await y.get('test');
   let a2 = await y.get('hello');
   console.log(`test: ${a1} -- ${a1==0xff2b}`);
-  console.log(`C2-r: ${a2} -- ${a2=='world'}`);
+  console.log(`hello: ${a2} -- ${a2=='world'}`);
 })();
